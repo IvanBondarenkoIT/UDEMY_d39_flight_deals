1f35d5c0d19a1b5df79ce9d5a9829e95cb37531f
Ivan <ivan.bondarenko.it@gmail.com>
2023-05-03 11:15:15 +0400

    data_manager: read_google_sheet done

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,25 @@
+import config
+import os
+import datetime
+import requests
+
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        pass
+        # self.USERNAME = os.environ.get("USER")
+        # self.PROJECT_NAME = os.environ.get("PROJECT")
+        # self.SHEETY_NAME = os.environ.get("SHEETY")
+        # self.SHEET_PASSWORD = os.environ.get("Authorization")
+
+    def read_google_sheet(self):
+        sheet_endpoint = f'https://api.sheety.co/{config.USER}/{config.PROJECT}/{config.SHEETY}'
+
+        bearer_headers = {
+            'Authorization': f'Bearer {config.Authorization}',
+        }
+
+        sheet_response = requests.get(sheet_endpoint, headers=bearer_headers)
+
+        return sheet_response.json()
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+
+
+def main():
+    dm = DataManager()
+    print(dm.read_google_sheet())
+
+
+if __name__ == '__main__':
+    main()
