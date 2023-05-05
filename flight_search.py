1235e987a69d9c500e19d1501557c68395950c39
Ivan <ivan.bondarenko.it@gmail.com>
2023-05-05 18:53:45 +0400

    search for cheap flights done

--- flight_search.py
+++ flight_search.py
@@ -1,16 +1,20 @@
 import config
 import requests
+import datetime
+from flight_data import FlightData
 
 
 class FlightSearch:
-    # This class is responsible for talking to the Flight Search API.
+    """This class is responsible for talking to the Flight Search API."""
     def __init__(self):
-        self.TEQUILA_KIWI_API = config.TEQUILA_KIWI_API
+        self.TEQUILA_KIWI_API_TOKEN = config.TEQUILA_KIWI_API_TOKEN
         self.TEQUILA_SERVER = config.TEQUILA_SERVER
+        self.API_CALL_QUERY = config.API_CALL_QUERY
+        self.API_CALL_SEARCH = config.API_CALL_SEARCH
 
     def get_iata_code(self, city_name: str):
-        tequila_endpoint = f'{self.TEQUILA_SERVER}/locations/query'
-        tequila_header = {'apikey': self.TEQUILA_KIWI_API}
+        tequila_endpoint = f'{self.TEQUILA_SERVER}/{self.API_CALL_QUERY}'
+        tequila_header = {'apikey': self.TEQUILA_KIWI_API_TOKEN}
 
         tequila_query = {
             "term": city_name,
@@ -25,3 +29,56 @@ class FlightSearch:
         results = response.json()["locations"]
         code = results[0]["code"]
         return code
+
+    def searching_for_flight(self, destination_city_code: str):
+        tequila_endpoint = f'{self.TEQUILA_SERVER}/{self.API_CALL_SEARCH}'
+
+        tequila_header = {
+            'apikey': self.TEQUILA_KIWI_API_TOKEN
+        }
+
+        date_tomorrow = datetime.datetime.now() + datetime.timedelta(days=1)
+        date_half_year_future = datetime.datetime.now() + datetime.timedelta(days=6 * 30)
+
+        tequila_query = {
+            "fly_from": config.HOME_CODE,
+            "fly_to": destination_city_code,
+            "dateFrom": date_tomorrow.strftime("%d/%m/%Y"),
+            "dateTo": date_half_year_future.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "USD"
+        }
+
+
+
+        response = requests.get(url=tequila_endpoint,
+                                headers=tequila_header,
+                                params=tequila_query)
+
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+
+        print(f"{flight_data.destination_city}: £{flight_data.price}")
+        return flight_data
+        #
+        # results = response.json()["locations"]
+        # code = results[0]["code"]
+
+        # {price: '', departure_airport_code: '', departure_city: ''}
--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import FlightData
 
 
 def main():
-    dm = DataManager()
+    """This file will need to use the DataManager,FlightSearch,
+     FlightData, NotificationManager classes to achieve the program requirements."""
+    # dm = DataManager()
     # sheet_data = dm.read_google_sheet()
     # print(sheet_data)
 
-    sheet_data = {'prices': [{'city': 'Paris', 'iataCode': '', 'lowestPrice': 54, 'id': 2},
-                {'city': 'Berlin', 'iataCode': 'LCY', 'lowestPrice': 42, 'id': 3},
-                {'city': 'Tokyo', 'iataCode': '', 'lowestPrice': 485, 'id': 4},
-                {'city': 'Sydney', 'iataCode': '', 'lowestPrice': 551, 'id': 5},
-                {'city': 'Istanbul', 'iataCode': '', 'lowestPrice': 95, 'id': 6},
-                {'city': 'Kuala Lumpur', 'iataCode': '', 'lowestPrice': 414, 'id': 7},
-                {'city': 'New York', 'iataCode': '', 'lowestPrice': 240, 'id': 8},
-                {'city': 'San Francisco', 'iataCode': '', 'lowestPrice': 260, 'id': 9},
-                {'city': 'Cape Town', 'iataCode': '', 'lowestPrice': 378, 'id': 10}]}
+    sheet_data = {'prices': [{'city': 'Paris', 'iataCode': 'PAR', 'lowestPrice': 54, 'id': 2}, {'city': 'Berlin', 'iataCode': 'LCY', 'lowestPrice': 42, 'id': 3}, {'city': 'Tokyo', 'iataCode': 'TYO', 'lowestPrice': 485, 'id': 4}, {'city': 'Sydney', 'iataCode': 'SYD', 'lowestPrice': 551, 'id': 5}, {'city': 'Istanbul', 'iataCode': 'IST', 'lowestPrice': 95, 'id': 6}, {'city': 'Kuala Lumpur', 'iataCode': 'KUL', 'lowestPrice': 414, 'id': 7}, {'city': 'New York', 'iataCode': 'NYC', 'lowestPrice': 240, 'id': 8}, {'city': 'San Francisco', 'iataCode': 'SFO', 'lowestPrice': 260, 'id': 9}, {'city': 'Cape Town', 'iataCode': 'CPT', 'lowestPrice': 378, 'id': 10}, {'city': 'Batumi', 'iataCode': 'BUS', 'lowestPrice': 500, 'id': 11}]}
+
     print(sheet_data)
     for row in sheet_data['prices']:
+        fs = FlightSearch()
         if row['iataCode'] == '':
-            row['iataCode'] = FlightSearch().get_iata_code(row['city'])
+            row['iataCode'] = fs.get_iata_code(row['city'])
             print(row['iataCode'])
-    print(sheet_data)
+
+        fs.searching_for_flight(row['iataCode'])
+    print(f'RESULT:\n{sheet_data}')
+
+
+
 
     # have_new_prices = {}
     # if have_new_prices:
